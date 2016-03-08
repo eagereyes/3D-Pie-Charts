faa8da46846c7aac123086bb3dcc35a22a4b2e9a
Robert Kosara <rkosara@me.com>
2016-03-08 09:34:28 -0800

    Add simple submit script

---
+++ study/submit_csv.php
@@ -0,0 +1,13 @@
+<?php
+
+$resultID = rtrim($_POST['resultID']);
+$data = stripslashes($_POST['data']);
+
+$dir = "../data/";
+
+$fh = fopen($dir . $resultID . ".csv", 'w') or die("Error opening file!");
+fwrite($fh, $data);
+fclose($fh);
+
+echo "Success";
+?>
