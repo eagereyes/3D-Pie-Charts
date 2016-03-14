661d475c520633c70231ee41d52662d9f32febb6
Robert Kosara <rkosara@tableau.com>
2016-03-14 16:59:57 -0700

    Lots of updates and cleanup

--- 3dpiemodel+analysis.R
+++ 3dpiemodel+analysis.R
@@ -2,91 +2,113 @@ library('pracma')
 library('dplyr')
 library('ggplot2')
 
+# Turn degrees into radians
+rad <- function(degrees) {
+	degrees/180*pi;
+}
+
 # Return partially applied function calculating arc length as
 # function of t for ellipse with axes a and b.
 ellipseArc <- function(a, b) {
-  function(t) c(a*cos(t), b*sin(t))
+	function(t) c(a*cos(t), b*sin(t))
 }
 
-# Calculate arc length of ellipse from angle rho to phi
+# Calculate arc length of ellipse from angle rho to theta
 # for ellipse with axes a and b using numerical integration
 # Uses pracma package http://www.inside-r.org/packages/cran/pracma/docs/arclength
-ellipseArcLength <- function(a, b, phi, rho = 0) {
-  arclength(ellipseArc(1, .5), rho, rho+phi, tol = 1e-10)$length
+ellipseArcLength <- function(a, b, theta, rho = 0) {
+	arclength(ellipseArc(a, b), rho, rho+theta, tol = 1e-10)$length
 }
 
 # Calculate ellipse area for a given angle by summing up quarters first
 # then adding the remaining portion
-ellipseAreaAux <- function(a, b, phi) {
-  area <- 0
-  quarters <- 0
-  while (phi > pi/2) {
-    area = area + a*b*pi/4
-    phi = phi - pi/2
-    quarters = quarters + 1
-  }
-  
-  if (quarters %% 2 == 0) {
-    area + a*b*pi/4 - .5*a*b*atan(a*tan(pi/2-phi)/b)
-  } else {
-    area + .5*a*b*atan(a*tan(phi)/b)
-  }
+ellipseAreaAux <- function(a, b, theta) {
+	area <- 0
+	quarters <- 0
+	while (theta > pi/2) {
+		area = area + a*b*pi/4
+		theta = theta - pi/2
+		quarters = quarters + 1
+	}
+	
+	if (quarters %% 2 == 0) {
+		area + a*b*pi/4 - .5*a*b*atan(a*tan(pi/2-theta)/b)
+	} else {
+		area + .5*a*b*atan(a*tan(theta)/b)
+	}
 }
 
 # Calculate area for ellipse with axes a and b for a slice with
-# angle phi and rotation rho.
-ellipseArea <- function(a, b, phi, rho = 0) {
-  ellipseAreaAux(a, b, rho+phi) - ellipseAreaAux(a, b, rho)
+# angle theta and rotation rho.
+ellipseAreaSlice <- function(a, b, theta, rho = 0) {
+	ellipseAreaAux(a, b, rho+theta) - ellipseAreaAux(a, b, rho)
+}
+
+ellipseArea <- function(a, b) {
+	a*b*pi
+}
+
+# Calculate the fraction of the ellipse area taken up by slice with angle theta and rotation rho 
+ellipseAreaFraction <- function(a, b, theta, rho) {
+	ellipseAreaSlice(a, b, theta, rho) / ellipseArea(a, b)
 }
 
 # Calculate the projected angles for a circle tilted at viewAngle alpha from the viewer
-# Returns the projected centralAngle phi and projected rotationAngle rho
-projectAngle <- function(alpha, phi, rho) {
-  xA <- cos(rho)
-  yA <- sin(rho)
-  
-  xB = cos(rho+phi)
-  yB = sin(rho+phi)
-  
-  yAProj <- yA*sin(alpha)
-  yBProj <- yB*sin(alpha)
-  
-  rotationProj <- atan2(yAProj, xA)
-  centralProj <- atan2(yBProj, xB) - rotationProj
-  
-  # Avoid the π to -π discontinuity when we cross the negative x axis (also keep angles in [0..2π])
-  if (rotationProj < 0) {
-    rotationProj = rotationProj + 2*pi
-  }
-  
-  if (centralProj < 0) {
-    centralProj = centralProj + 2*pi
-  }
-  
-  list(phiProj=centralProj, rhoProj = rotationProj)
+# Returns the projected centralAngle theta and projected rotationAngle rho
+projectAngle <- function(alpha, theta, rho) {
+	xA <- cos(rho)
+	yA <- sin(rho)
+	
+	xB = cos(rho+theta)
+	yB = sin(rho+theta)
+	
+	yAProj <- yA*sin(alpha)
+	yBProj <- yB*sin(alpha)
+	
+	rotationProj <- atan2(yAProj, xA)
+	centralProj <- atan2(yBProj, xB) - rotationProj
+	
+	# Avoid the π to -π discontinuity when we cross the negative x axis (also keep angles in [0..2π])
+	rotationProj <- ifelse(rotationProj < 0, rotationProj + 2*pi, rotationProj)
+
+	centralProj <- ifelse(centralProj < 0,  centralProj + 2*pi, centralProj)
+	
+	list(thetaProj=centralProj, rhoProj = rotationProj)
 }
 
 makePredictions <- function() {
-  angles <- sapply(1:359, deg2rad)
-  area <- sapply(angles, function(phi) { ellipseArea(1, .5, phi)})
-  arc <- sapply(angles, function(phi) { ellipseArcLength(1, .5, phi)})
-  predictions <- data.frame(angles, arc, area)
+	angles <- sapply(1:359, deg2rad)
+	area <- sapply(angles, function(theta) { ellipseAreaSlice(1, .5, theta)})
+	arc <- sapply(angles, function(theta) { ellipseArcLength(1, .5, theta)})
+	predictions <- data.frame(angles, arc, area)
   
-  return(predictions)
+	return(predictions)
 }
 
 setwd('/Users/rkosara/Dropbox (Tableau)/Research/3D Pie Charts')
-data <- read.csv('pilot-data.csv')
-data <- transform(data, logError = log2(abs(answer-value)+1/8),
-                  viewAngle = factor(viewAngle))
+data <- read.csv('results-3dpiestudy.csv')
+
+projections <- with(data, projectAngle(rad(viewAngle), rad(centralAngle), rad(rotation)))
+data <- cbind(data, projections)
+
+data <- within(data, {
+				logError = log2(abs(answer-value)+1/8)
+				verticalFactor = sin(rad(viewAngle))
+				areaFraction = ellipseAreaFraction(1, verticalFactor, thetaProj, rhoProj)
+				viewAngle = factor(viewAngle)
+				})
 
 dataAggregated = data %>%
-  group_by(viewAngle, resultID) %>%
-  summarize(meanError = mean(logError))
+	group_by(viewAngle, resultID, condition) %>%
+	summarize(meanError = mean(logError))
 
-ggplot(dataAggregated, aes(x=viewAngle, fill=factor(viewAngle))) +
-     geom_violin(size=1, aes(
-         y=meanError,
-         color=factor(viewAngle)),
-         show.legend = FALSE) +
-     labs(x = "View Angle", y = "Log Error")
+# Error by view Angle for the two conditions
+ggplot(dataAggregated, aes(x=viewAngle, y=meanError, fill=factor(viewAngle))) +
+	geom_violin(size=1, aes(y=meanError, color=factor(viewAngle)), show.legend=FALSE) +
+	stat_summary(fun.y=mean, geom="point", shape=5, size=4, show.legend=FALSE) +
+	labs(x = "View Angle", y = "Log Error") + facet_grid(condition ~ .)
+
+# t test between the aggregated means for condition 1 and 2
+cond1 <- subset(dataAggregated, condition=="cond1")$meanError
+cond2 <- subset(dataAggregated, condition=="cond2")$meanError
+t.test(cond1, cond2)
