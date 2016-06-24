5b484dd00169daf37343ba0e5ff583733584fb84
Robert Kosara <rkosara@tableau.com>
2016-06-24 15:14:07 -0700

    Draw 3D pie as two cylinders in three.js/webGL

--- study/threedeepies-threejs.js
+++ study/threedeepies-threejs.js
@@ -3,22 +3,38 @@ function initGL(domContainer, width, height) {
 	var glInfo = {};
 
 	glInfo.scene = new THREE.Scene();
-	glInfo.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+	glInfo.camera = new THREE.PerspectiveCamera(25, width/height, 0.1, 1000);
 
 	glInfo.renderer = new THREE.WebGLRenderer();
 	glInfo.renderer.setSize(width, height);
+	glInfo.renderer.setClearColor(0xffffff);
+	glInfo.renderer.context.disable(glInfo.renderer.context.DEPTH_TEST);
+
 	domContainer.appendChild(glInfo.renderer.domElement);
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	var cube = new THREE.Mesh(geometry, material);
+	var geometry = new THREE.CylinderBufferGeometry(1, 1, .02, 100);
+	var material = new THREE.MeshBasicMaterial({color: 0xd3d3d3}); // lightgray
+	glInfo.baseDisk = new THREE.Mesh(geometry, material);
 
-	glInfo.scene.add(cube);
+	geometry = new THREE.CylinderBufferGeometry(1, 1, .02, 100, 1, false, 0, rad(30));
+
+	glInfo.wedgeMaterial = new THREE.MeshBasicMaterial({color: 0x4682b4}); // steelblue
+
+	glInfo.wedge = new THREE.Mesh(geometry, glInfo.wedgeMaterial);
+	glInfo.wedge.rotation.y = rad(220+90);
+
+	glInfo.baseDisk.add(glInfo.wedge);
+
+	glInfo.scene.add(glInfo.baseDisk);
+
 	glInfo.camera.position.z = 5;
 
 	return glInfo;
 }
 
-function renderGL(glInfo) {
+function drawGLPie(glInfo, centralAngle, viewAngle, rotation, radius, body) {
+	glInfo.baseDisk.rotation.x = viewAngle;
+	glInfo.wedge.rotation.y = rotation+Math.PI/2;
+
 	glInfo.renderer.render(glInfo.scene, glInfo.camera);
 }
